6328b865c5c0dc666bbc72c123419a8bf01d8052
Antti Haarala <anttip.haarala@gmail.com>
2022-01-03 19:58:20 +0200

    create crud functions

--- backend-azure-dot-net/posts.cs
+++ backend-azure-dot-net/posts.cs
@@ -12,10 +12,93 @@ namespace Posts.Function
 {
     public static class posts
     {
-        [FunctionName("posts")]
-        public static async Task<IActionResult> Run(
-            [HttpTrigger(AuthorizationLevel.Function, "get", "post", Route = null)] HttpRequest req,
+        [FunctionName("getPosts")]
+        public static async Task<IActionResult> getPosts(
+            [HttpTrigger(AuthorizationLevel.Function, "get", Route = "post")] HttpRequest req,
             ILogger log)
+        {
+            log.LogInformation("C# HTTP trigger function processed a request.");
+
+            string name = req.Query["name"];
+
+            string requestBody = await new StreamReader(req.Body).ReadToEndAsync();
+            dynamic data = JsonConvert.DeserializeObject(requestBody);
+            name = name ?? data?.name;
+
+            string responseMessage = string.IsNullOrEmpty(name)
+                ? "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response."
+                : $"Hello, {name}. This HTTP triggered function executed successfully.";
+
+            return new OkObjectResult(responseMessage);
+        }
+
+        [FunctionName("getPost")]
+        public static async Task<IActionResult> getPost(
+            [HttpTrigger(AuthorizationLevel.Function, "get", Route = "post/{id}")] HttpRequest req,
+            ILogger log,
+            string id)
+        {
+            log.LogInformation("C# HTTP trigger function processed a request.");
+
+            string name = req.Query["name"];
+
+            string requestBody = await new StreamReader(req.Body).ReadToEndAsync();
+            dynamic data = JsonConvert.DeserializeObject(requestBody);
+            name = name ?? data?.name;
+
+            string responseMessage = string.IsNullOrEmpty(name)
+                ? "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response."
+                : $"Hello, {name}. This HTTP triggered function executed successfully.";
+
+            return new OkObjectResult(responseMessage);
+        }
+
+        [FunctionName("postPost")]
+        public static async Task<IActionResult> postPost(
+            [HttpTrigger(AuthorizationLevel.Function, "post", Route = "post")] HttpRequest req,
+            ILogger log)
+        {
+            log.LogInformation("C# HTTP trigger function processed a request.");
+
+            string name = req.Query["name"];
+
+            string requestBody = await new StreamReader(req.Body).ReadToEndAsync();
+            dynamic data = JsonConvert.DeserializeObject(requestBody);
+            name = name ?? data?.name;
+
+            string responseMessage = string.IsNullOrEmpty(name)
+                ? "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response."
+                : $"Hello, {name}. This HTTP triggered function executed successfully.";
+
+            return new OkObjectResult(responseMessage);
+        }
+
+        [FunctionName("deletePost")]
+        public static async Task<IActionResult> deletePost(
+            [HttpTrigger(AuthorizationLevel.Function, "delete", Route = "post/{id}")] HttpRequest req,
+            ILogger log,
+            string id)
+        {
+            log.LogInformation("C# HTTP trigger function processed a request.");
+
+            string name = req.Query["name"];
+
+            string requestBody = await new StreamReader(req.Body).ReadToEndAsync();
+            dynamic data = JsonConvert.DeserializeObject(requestBody);
+            name = name ?? data?.name;
+
+            string responseMessage = string.IsNullOrEmpty(name)
+                ? "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response."
+                : $"Hello, {name}. This HTTP triggered function executed successfully.";
+
+            return new OkObjectResult(responseMessage);
+        }
+
+        [FunctionName("modifyPost")]
+        public static async Task<IActionResult> modifyPost(
+            [HttpTrigger(AuthorizationLevel.Function, "put", Route = "post/{id}")] HttpRequest req,
+            ILogger log,
+            string id)
         {
             log.LogInformation("C# HTTP trigger function processed a request.");
 
